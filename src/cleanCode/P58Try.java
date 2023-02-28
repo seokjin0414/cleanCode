82a2e46315a7a6d5bf15afb244f12217c6e0f437
seokjin0414 <sars21@hanmail.net>
2023-02-28 10:43:16 +0900

    1st

--- src/cleanCode/P58Try.java
+++ src/cleanCode/P58Try.java
@@ -1,15 +1,5 @@
 package cleanCode;
 
 public class P58Try {
-    try {
-        deletePage(page);
-        registry.deleteReference(page.name);
-        configKeys■deleteKey(page.name.makeKey());
-    }
-    catch (Exception e)
-
-    {
-        System.out.println(e.getMessage());
-    }
 
     }
